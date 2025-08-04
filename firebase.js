d7371b59f75ff41c042289aac19a49d4676476c8
Nikhilroy22 <nikhilroyfff@gmail.com>
2025-08-04 22:13:28 +0600

    first commit

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./nde.json");
+const serviceAccount = require("./nroy-7cc2e-firebase-adminsdk-fbsvc-375c2a3d30.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
